3d33ca67fb91f1a6b104eab98076d3fbd9256993
GNEHS <jayuiop@gmail.com>
2018-01-26 14:20:25 +0800

    修正圖片無法觀看之錯誤、美化

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.ripple(".items a.item,.menu a.item,.ts.button,a.card", {
+    $.ripple(".items a.item,.ts.button,a.card", {
         debug: false, // Turn Ripple.js logging on/off
         on: 'mousedown', // The event to trigger a ripple effect
 
@@ -16,14 +16,40 @@ $(document).ready(function() {
 
         easing: 'linear' // The CSS3 easing function of the ripple
     });
+    $.ripple(".menu a.item", {
+        debug: false, // Turn Ripple.js logging on/off
+        on: 'mousedown', // The event to trigger a ripple effect
+
+        opacity: 0.4, // The opacity of the ripple
+        color: "auto", // Set the background color. If set to "auto", it will use the text color
+        multi: true, // Allow multiple ripples per element
+
+        duration: 0.3, // The duration of the ripple
+
+        // Filter function for modifying the speed of the ripple
+        rate: function(pxPerSecond) {
+            return pxPerSecond;
+        },
+
+        easing: 'linear' // The CSS3 easing function of the ripple
+    });
     $('#search').keypress(function(e) {
         if (e.which == 13) {
             search()
         }
     });
-    $('#content td').attr("style", "")
+
     $('#content table').attr("style", "")
     $('#content table').attr("class", "ts celled table")
+    $('#content td').attr("style", "")
+
+    $('#content img').attr('src', function() {
+        var osrc = $(this).attr('src')
+        console.log(osrc)
+        return 'http://web.tlhc.ylc.edu.tw' + osrc
+    })
+    $('#content img').attr("style", "")
+    $('#content img').attr("class", "ts centered rounded image")
 });
 
 function search() {
