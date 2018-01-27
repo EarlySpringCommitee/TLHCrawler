d1caa0c600aac82c9dfbc69583a7a1fe6675a926
GNEHS <jayuiop@gmail.com>
2018-01-27 10:03:46 +0800

    修改 <a> 沒覆蓋到機掰的 <span>、分數部分 js 優化

--- js/score.js
+++ js/score.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
 
     $("#score ,#total").attr('style', '')
-    $("#score tr:first-child ,#total tr:last-child").remove();
+    $("#score tr:first-child ,#total tr:last-child").remove()
 
     $('#score td ,#total td').html(function() {
         var text = $(this).text()
         if (text < 60 && text > 0) {
             $(this).addClass('negative')
+                // 不及格
         }
-        if (text < 101 && text > 0) {
+        if (text <= 100 && text >= 0) {
             $(this).addClass('score')
+                // 如果是分數，加上等寬字元
         }
         return text
     })
