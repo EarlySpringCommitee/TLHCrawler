d70322784a7b31298aafd804969854239c337de3
GNEHS <jayuiop@gmail.com>
2018-03-10 18:48:57 +0800

    分離首頁可自訂部分

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require('fs'); //檔案系統
 const tlhcRequest = require('./TLHCrequest.js'); //因為程式碼太長分出來的模塊
 const tlhcScore = require('./tlhcScore.js'); //因為程式碼太長分出來的模塊
+const config = require('./config.js'); //因為程式碼太長分出來的模塊
 const excerpt = require("html-excerpt"); // 取摘要
 const request = require("request"); // HTTP 客戶端輔助工具
 const cheerio = require("cheerio"); // Server 端的 jQuery 實作
@@ -53,77 +54,10 @@ app.get('/og/og.png', (req, res) => {
 });
 //------------可愛的首頁------------
 app.get('/', (req, res) => {
-    links = [{
-        "name": "學校佈告欄",
-        "table": [{
-            'header': '校園公告',
-            'description': '好像很重要，又沒那麼重要',
-            'link': '/tlhc/pages/40-1001-15-1.php'
-        }, {
-            'header': '榮譽榜',
-            'description': '實際上沒人去看的東西，只會害你升旗站更久',
-            'link': '/tlhc/pages/40-1001-38-1.php'
-        }, {
-            'header': '轉知資訊-政令宣導',
-            'description': '一些比賽跟你完全不會想點進去看的東西',
-            'link': '/tlhc/pages/40-1001-29-1.php'
-        }, {
-            'header': '圖書館公告',
-            'description': '消失在學校網頁上ㄉ圖書館公告',
-            'link': '/tlhc/pages/40-1001-21-1.php'
-        }, {
-            'header': '獎助學金公告',
-            'description': '能拿錢的情報',
-            'link': '/tlhc/pages/40-1001-30-1.php'
-        }, {
-            'header': '教務處公告',
-            'description': '招生考試之類的',
-            'link': '/tlhc/pages/40-1001-28-1.php'
-        }]
-    }, {
-        "name": "一些你可能用到的相關連結",
-        "table": [{
-            'header': '行事曆',
-            'description': '可愛的行事曆',
-            'link': 'http://web.tlhc.ylc.edu.tw/ezfiles/1/1001/img/7/161383665.pdf'
-        }, {
-            'header': 'Web 學生選課系統',
-            'description': '不安全 Desu',
-            'link': 'http://register.tlhc.ylc.edu.tw/stdcourse3/'
-        }, {
-            'header': '設備報修',
-            'description': '設計得很有障礙的表格',
-            'link': 'https://docs.google.com/forms/d/e/1FAIpQLSe1ex8qCBmaWWR0BwK4felr_BI4eV2CJ1bM0eng71OfrsUKsw/viewform'
-        }, {
-            'header': '學生社團選填',
-            'description': '我賭一百隻長頸鹿跟成績系統同一家出的',
-            'link': 'http://register.tlhc.ylc.edu.tw/clgstd/Index.aspx'
-        }, {
-            'header': '志工服務管理系統',
-            'description': '跟成績系統同一家的系統',
-            'link': 'http://163.27.124.31/EPSTD/'
-        }, {
-            'header': '首頁',
-            'description': '對，就是學校首頁',
-            'link': 'http://web.tlhc.ylc.edu.tw/bin/home.php'
-        }, {
-            'header': '雲端社群播客(哀居踢)',
-            'description': '幹不要把文字放進圖片',
-            'link': 'http://igtplus.tlhc.ylc.edu.tw/'
-        }]
-    }]
-    slide = [{
-        'header': '狗狗',
-        'description': '對，就是狗狗',
-        'img': '//i.imgur.com/lOETuw6.jpg',
-        'link': 'http://web.tlhc.ylc.edu.tw/bin/home.php'
-    }, {
-        'header': '還是狗狗',
-        'description': '對，就是狗狗',
-        'img': '//i.imgur.com/lOETuw6.jpg',
-        'link': 'http://web.tlhc.ylc.edu.tw/bin/home.php'
-    }]
-    res.render('index', { title: 'ㄉㄌㄐㄕ - 公告' })
+    let links = config.links
+    let slide = config.slide
+    console.log(slide)
+    res.render('index', { title: 'ㄉㄌㄐㄕ - 公告', links: links, slide: slide })
 })
 app.get('/about/', (req, res) => {
     res.render('about', { title: 'ㄉㄌㄐㄕ - 關於' });
