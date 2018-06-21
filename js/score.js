fe2d1f5a92a9a43d71af564d5ba17b5a7fb19425
gnehs <jayuiop@gmail.com>
2018-06-21 20:53:53 +0800

    修正判斷

--- js/score.js
+++ js/score.js
@@ -19,6 +19,9 @@ $(document).ready(function() {
     $("[data-table] tr:last-child").remove()
     $("[data-table=\"rank\"]").removeClass("first line")
     $("[data-table=\"score\"] tr:first-child,[data-table=\"score\"] td:nth-child(4),[data-table=\"score\"] td:nth-child(7)").remove()
+    $("[data-table=\"total\"] td:nth-child(4),[data-table=\"total\"] td:nth-child(7)").remove()
+    $("[data-table=\"total\"] tr:nth-child(1n+2) td:nth-child(1n+2)").addClass("score")
+    $("[data-table=\"rank\"] tr td:nth-child(1n+2)").addClass("score")
     $("[data-table=\"rewards\"] tr:first-child").remove()
     $("[data-table=\"group\"] tr td:last-child").remove()
     $('td').removeAttr('class')
@@ -53,7 +56,7 @@ $(document).ready(function() {
         }
         return text
     })
-    $('[data-table=\"rank\"] td').html(function() {
+    $('[data-table=\"rank\"] td,[data-table=\"total\"] td').html(function() {
         var text = $(this).text().replace(/[s]+/g, "");
         if (text.match('成績輸入期間')) {
             var text = "";
