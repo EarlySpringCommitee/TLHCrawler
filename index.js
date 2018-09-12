dea26871a7a58e17cc141b2dffaaf2a0865de69e
gnehs <you@example.com>
2018-09-12 20:37:07 +0800

    修正正規表達式

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('/tlhc/pages/:id', (req, res) => {
 
 app.get('/tlhc/post/:id', (req, res) => {
     let url = Base64.decode(req.params.id)
-    if (url.match(/[0-9]*-[0-9]*-[0-9]*,r[0-9]*-[0-9]*\.php/)) {
+    if (url.match(/[0-9]*-[0-9]*-[0-9]*.+\.php/)) {
         var originalURL = "http://web.tlhc.ylc.edu.tw/files/" + url
         tlhcRequest.getPost(originalURL, Base64.decode(req.params.id), res)
     } else {
