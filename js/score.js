610455ded5477d385b593bc67f93a1c30494f12e
gnehs <you@example.com>
2018-12-19 22:26:58 +0800

    優化前後端表格處理

--- js/score.js
+++ js/score.js
@@ -1,45 +1,78 @@
 $(document).ready(function () {
-    $("table").removeAttr('style')
+    function removeScoreProcessing(el) {
+        $(el).html(function () {
+            let text = $(this).text().trim();
+            return (text.match(/成績輸入期間|成績處理期間/)) ? "" : text
+        })
+    }
+
+    function scoreRating(el) {
+        $(el).html(function () {
+            let text = $(this).text().trim();
+            if (text < 60 && text > 0 || text == '0') {
+                // 不及格
+                $(this).addClass('negative')
+            }
+            if (text <= 100 && text >= 80) {
+                // 八十分
+                $(this).addClass('positive')
+            }
+            if (text <= 100 && text >= 0) {
+                // 如果是分數，加上等寬字元
+                $(this).addClass('score')
+            }
+            return text
+        })
+    }
+
+    function othersRating(el) {
+        $(el).html(function () {
+            let text = $(this).text().trim();
+            if (text.match(/大功|小功|嘉獎/)) {
+                // 棒棒
+                $(this).addClass('positive')
+            }
+            if (text.match(/曠課|遲到|升降旗缺席|小過|警告|缺點/)) {
+                // 壞壞
+                $(this).addClass('negative')
+            }
+            return text
+        })
+    }
+
     $("[data-table] tr:last-child").remove()
-    $("[data-table=\"rank\"]").removeClass("first line")
+    $("[data-table=\"semesterRank\"]").removeClass("first line")
     $('td').removeAttr('class')
-    $(`[data-table="rank"] tr td:nth-child(1n+2)`).addClass("score")
-    $(`[data-table="total"] tr td:nth-child(1n+2)`).addClass("score")
-    $('[data-table=\"score\"] td,[data-table=\"day\"] td,[data-table=\"rewards\"] td').html(function () {
-        var text = $(this).text().trim();
-        if (text < 60 && text > 0 || text == '0') {
-            // 不及格
-            $(this).addClass('negative')
-        }
-        if (text <= 100 && text >= 80) {
-            // 八十分
-            $(this).addClass('positive')
-        }
-        if (text <= 100 && text >= 0) {
-            // 如果是分數，加上等寬字元
-            $(this).addClass('score')
-        }
-        if (text.match(/大功|小功|嘉獎/)) {
-            // 棒棒
-            $(this).addClass('positive')
-        }
-        if (text.match(/曠課|遲到|升降旗缺席|小過|警告|缺點/)) {
-            // 壞壞
-            $(this).addClass('negative')
-        }
-        if (text.match(/成績輸入期間|成績處理期間/)) {
-            var text = "";
-        }
-        return text
+    $('td').html(function () {
+        return $(this).text().trim()
     })
-    $(`[data-table="score"] td:last-child`).removeClass('negative positive') //移除學分上的正負面 class
-    $('[data-table="rank"] td,[data-table="total"] td').html(function () {
-        var text = $(this).text().trim();
-        if (text.match(/成績輸入期間|成績處理期間/)) {
-            return ""
-        }
-        return text
-    })
+
+    /*/ === 移除「成績輸入期間、成績處理期間」 === /*/
+    removeScoreProcessing(`[data-table="score"] td`) //本學期段考成績
+    removeScoreProcessing(`[data-table="rank"] td`) //本學期段考排名
+    removeScoreProcessing(`[data-table="semesterRank"] td`) //學期排名
+    removeScoreProcessing(`[data-table="semesterScore"] td`) //學期總成績
+
+    /*/ === 分數閃亮亮 === /*/
+    // 本學期段考成績
+    scoreRating(`[data-table="score"] td`)
+    // 本學期段考排名
+    scoreRating(`[data-table="rank"] tr:nth-child(2) td:nth-child(n+2)`)
+    $(`[data-table="rank"] tr:nth-child(n+1) td:nth-child(n+2)`).addClass("score")
+    // 學期排名
+    scoreRating(`[data-table="semesterRank"] tr:nth-child(1) > td.score`)
+    $(`[data-table="semesterRank"] tr td:nth-child(1n+2)`).addClass("score")
+    // 學期總成績
+    scoreRating(`[data-table="semesterScore"] td:nth-child(n+3):not(:nth-child(n+7))`) // 略過學分
+    $(`[data-table="semesterScore"] td:nth-child(n+7)`).addClass("score") // 學分加上等寬
+
+    /*/ === 獎懲、出勤閃亮亮 === /*/
+    othersRating(`[data-table="rewards"] td`) // 獎懲
+    othersRating(`[data-table="day"] td`) // 出勤
+
+    /*/ === 表格整理完畢，耶！ === /*/
+    $("table").removeAttr('style')
+
     // 匯出資料
     if ($("table")) {
         let downloadDiv = `<div class="mdui-list">`
