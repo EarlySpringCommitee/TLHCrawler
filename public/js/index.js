4d5c9b07efb6c2edd98429d760b30d649ec1267b
gnehs <jayuiop@gmail.com>
2020-05-27 19:56:23 +0800

    Fixed calendar api key

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -7,13 +7,13 @@ $(function() {
         },
         defaultView: 'listWeek',
         // 這個 API 金鑰寫了存取限制
-        googleCalendarApiKey: 'AIzaSyC97f3HHOnumbwC5eNkk9qGw4Qxs5SiVtw',
+        googleCalendarApiKey: 'AIzaSyCvRz-3t9sdMROZxhoz8PkBMOXrdHxYjLo',
         events: {
             googleCalendarId: '11001@tlhc.ylc.edu.tw',
             color: '#d86712', // an option!
             textColor: '#FFF' // an option!
         },
-        eventClick: function(event) {
+        eventClick: event => {
             // opens events in a popup window
             window.open(event.url, '_blank', 'width=700,height=600');
             return false;
